0aa1b26ee20ece36b470747804f4655042b56501
Nils Hartmann <nils@nilshartmann.net>
2025-05-18 11:08:37 +0200

    01: "Statische Teaser"-Komponente

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,5 @@
-import ArticleSlider from "./ArticleSlider.tsx";
-
-const article1 = {
-  id: "1",
-  likes: 421,
-  title: "Hello React",
-};
-
-const article2 = {
-  id: "2",
-  likes: 0,
-  title: "Hello Next.js",
-};
-
-const article3 = {
-  id: "3",
-  likes: 10,
-  title: "Hello JavaScript",
-};
-const article4 = {
-  id: "4",
-  likes: 10,
-  title: "Hello HTML",
-};
-const article5 = {
-  id: "5",
-  likes: 10,
-  title: "Hello TypeScript",
-};
+import ArticleTeaser from "./components/ArticleTeaser.tsx";
 
 export default function App() {
-  return (
-    <div>
-      <ArticleSlider
-        articles={[article1, article2, article3, article4, article5]}
-      />
-    </div>
-  );
+  return <ArticleTeaser />;
 }
---
+++ src/components/ArticleTeaser.tsx
@@ -0,0 +1,11 @@
+export default function ArticleTeaser() {
+  return (
+    <article>
+      <h1>Hello React</h1>
+      <p>
+        von <em>Nora Müller</em>
+      </p>
+      <p>Eine kurze und knackige Einführung in React</p>
+    </article>
+  );
+}
